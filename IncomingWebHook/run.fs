70fbf7519001da3722a4159c99144df596508de2
mikesigs <mikesigsworth@gmail.com>
2018-02-02 00:27:51 -0700

    Init

---
+++ IncomingWebHook/run.fs
@@ -0,0 +1,29 @@
+namespace WhoseTurnIsIt
+
+open System.Net
+open System.Net.Http
+open Newtonsoft.Json
+open Microsoft.Azure.WebJobs.Host
+
+type Name = {
+    First: string
+    Last: string
+}
+
+type Greeting = {
+    Greeting: string
+}
+
+module IncomingWebHook =
+    let Run(req: HttpRequestMessage, log: TraceWriter) =
+        async {
+            log.Info("Webhook was triggered!")
+            let! jsonContent = req.Content.ReadAsStringAsync() |> Async.AwaitTask
+
+            try
+                let name = JsonConvert.DeserializeObject<Name>(jsonContent)
+                return req.CreateResponse(HttpStatusCode.OK, 
+                    { Greeting = sprintf "Hello %s %s!" name.First name.Last })
+            with _ ->
+                return req.CreateResponse(HttpStatusCode.BadRequest)
+        } |> Async.StartAsTask
