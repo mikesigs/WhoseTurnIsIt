7f9a1ec5dbf43cc84326b47a2dfcee68c3b28e72
mikesigs <mikesigsworth@gmail.com>
2018-02-04 00:44:21 -0700

    More logging

--- IncomingWebHook/run.fsx
+++ IncomingWebHook/run.fsx
@@ -33,11 +33,13 @@ let Run(req: HttpRequestMessage, log: TraceWriter) =
     async {
         log.Info("Webhook was triggered yay!")
         let! jsonContent = req.Content.ReadAsStringAsync() |> Async.AwaitTask
-
+        log.Info jsonContent
         try
             let name = JsonConvert.DeserializeObject<Name>(jsonContent)
-            return req.CreateResponse(HttpStatusCode.OK, 
-                { Greeting = sprintf "Hello %s %s!" name.First name.Last })
+            log.Info (sprintf "%A" name)
+            let greeting = { Greeting = (sprintf "Hello %s %s!" name.First name.Last ) }
+            log.Info (sprintf "%A" greeting)
+            return req.CreateResponse(HttpStatusCode.OK, greeting)
         with ex ->
             log.Error (ex.Message)
             return req.CreateResponse(HttpStatusCode.BadRequest)
