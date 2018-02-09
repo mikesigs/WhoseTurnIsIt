9c5fa2d10514b63990c3450c3e787357e8964e36
mikesigs <mikesigsworth@gmail.com>
2018-02-09 13:34:56 -0700

    Added fake publish and run tasks and added my .vscode folder

--- src/Script.fsx
+++
@@ -1,4 +0,0 @@
-#load "WhoseTurnIsIt.fs"
-open WhoseTurnIsIt
-
-// Define your library scripting code here
--- src/WhoseTurnIsIt.fs
+++
@@ -1,6 +0,0 @@
-namespace WhoseTurnIsIt
-
-type WhoseTurnIsIt() = 
-    member this.X = "F#"
-
-
